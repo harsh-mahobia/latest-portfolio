1beb4bbd54612f03a1be0ed32046d9068dec5cc7
Harsh <harsh10mahobia@gmail.com>
2025-04-25 15:14:05 +0530

    changes

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,7 +2,7 @@ export const experiences = [
   {
     id: 1,
     title: 'Supervised and Unsupervised Algorithms',
-    company: "Programming Pathshala",
+    company: "Programming Pathshala - A program offered by Programming Pathshala to integrate AI into the Software Development Task",
     duration: ""
   },
 ]
