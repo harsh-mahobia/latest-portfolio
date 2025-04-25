3bf414d834ee2734c8b1b328f8edc08684d990f3
Harsh <harsh10mahobia@gmail.com>
2025-04-25 12:29:41 +0530

    changes

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,19 +1,13 @@
 "use client"
 
-import Lottie from "lottie-react";
 
 const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+  
 
   return (
-    <Lottie {...defaultOptions} />
+    <div className="w-full h-full flex justify-center items-center">
+     Hello
+    </div>
   );
 };
 
